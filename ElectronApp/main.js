39c3dd2a165ac04d8a2c5bee01161d6c370638e9
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2021-11-30 16:59:23 +0200

    Electron

---
+++ ElectronApp/main.js
@@ -0,0 +1,44 @@
+const {app, BrowserWindow} = require('electron');
+
+function createWindow() {
+
+    const win = new BrowserWindow({
+
+        width: 800,
+
+        height: 600,
+
+        webPreferences: {
+
+            nodeIntegration: true
+        }
+    })
+
+    win.loadURL('http://localhost:4200')
+
+    win.webContents.openDevTools()
+
+}
+
+app.commandLine.appendSwitch('ignore-certificate-errors')
+
+app.on('ready',
+    createWindow
+)
+
+app.on('window - all - closed',
+    () => {
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
+    }
+)
+
+app.on('activate',
+    () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+
+            createWindow()
+        }
+    }
+)
